5cf79a614a9c57052d3219d9bfb9b44927138ef8
Hyunjin Kim <ouiqdmw17@gmail.com>
2025-01-21 13:34:53 +0900

    fix zigzag

--- zigzag_cython/core.pyx
+++ zigzag_cython/core.pyx
@@ -43,7 +43,7 @@ def peak_valley_pivots(
     if not str(LOW.dtype).startswith('float'):
         LOW = LOW.astype(np.float64)
 
-    res, confirmed_idx = peak_valley_pivots_detailed(
+    res, confirmed_idx = peak_valley_pivots_detailed_fix(
         HIGH,
         LOW,
         cHIGH,
@@ -205,6 +205,124 @@ cpdef peak_valley_pivots_detailed(double [:] HIGH,
                         last_pivot_price = current_pivot_low
                         last_pivot_direction = -1
                         last_pivot_confirmed = 0
+
+
+    return pivots, pivot_confirmed_ats
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef peak_valley_pivots_detailed_fix(double [:] HIGH,
+                                  double [:] LOW,
+                                  double [:] cHIGH,
+                                  double [:] cLOW,
+                                  double min_dev,
+                                  int depth,
+                                  int allowed_zigzag_on_one_bar):
+
+    cdef:
+        int64_t t_n = len(HIGH)
+        ndarray[int64_t, ndim=1] pivots = np.zeros(t_n, dtype=np.int64)
+        ndarray[int64_t, ndim=1] pivot_confirmed_ats = np.zeros(t_n, dtype=np.int64)
+        int64_t last_pivot = -1
+        int64_t last_pivot_direction
+        double_t last_pivot_price
+        int64_t last_pivot_confirmed
+        int64_t peak_candidate
+        int64_t valley_candidate
+        # index types
+        int64_t t
+        int64_t current_pivot_target
+        int64_t i
+        double_t dev_threshold
+
+
+    for t in range(depth * 2, t_n):
+        dev_threshold = min_dev
+
+        peak_candidate = 1
+        current_pivot_target = t - depth
+        current_pivot_high = HIGH[current_pivot_target]
+        current_chigh = cHIGH[t]
+        for i in range(current_pivot_target - depth, current_pivot_target):
+            if HIGH[i] >= current_pivot_high:
+                peak_candidate = 0
+                break
+        for i in range(current_pivot_target + 1, current_pivot_target + depth + 1):
+            if HIGH[i] > current_pivot_high:
+                peak_candidate = 0
+                break
+        if peak_candidate == 1:
+            if last_pivot == -1:
+                last_pivot = current_pivot_target
+                last_pivot_direction = 1
+                last_pivot_price = current_pivot_high
+                last_pivot_confirmed = 0
+            else:
+                if last_pivot_direction == 1:
+                    if current_pivot_high > last_pivot_price:
+                        last_pivot = current_pivot_target
+                        last_pivot_price = current_pivot_high
+                        last_pivot_confirmed = 0
+                else:
+                    deviation_rate = (current_pivot_high - last_pivot_price) / last_pivot_price * 100
+                    if deviation_rate >= dev_threshold:
+                        last_pivot = current_pivot_target
+                        last_pivot_price = current_pivot_high
+                        last_pivot_direction = 1
+                        last_pivot_confirmed = 0
+
+        valley_candidate = 1
+        current_pivot_target = t - depth
+        current_pivot_low = LOW[current_pivot_target]
+        current_clow = cLOW[t]
+        for i in range(current_pivot_target - depth, current_pivot_target):
+            if LOW[i] <= current_pivot_low:
+                valley_candidate = 0
+                break
+        for i in range(current_pivot_target + 1, current_pivot_target + depth + 1):
+            if LOW[i] < current_pivot_low:
+                valley_candidate = 0
+                break
+        if last_pivot != -1 and last_pivot == current_pivot_target and allowed_zigzag_on_one_bar == 0:
+            valley_candidate = 0
+        if valley_candidate == 1:
+            if last_pivot == -1:
+                last_pivot = current_pivot_target
+                last_pivot_direction = -1
+                last_pivot_price = current_pivot_low
+                last_pivot_confirmed = 0
+            else:
+                if last_pivot_direction == -1:
+                    if current_pivot_low < last_pivot_price:
+                        last_pivot = current_pivot_target
+                        last_pivot_price = current_pivot_low
+                        last_pivot_confirmed = 0
+                else:
+                    deviation_rate = (last_pivot_price - current_pivot_low) / last_pivot_price * 100
+                    if deviation_rate >= dev_threshold:
+                        last_pivot = current_pivot_target
+                        last_pivot_price = current_pivot_low
+                        last_pivot_direction = -1
+                        last_pivot_confirmed = 0
+
+        if last_pivot != -1 and last_pivot_confirmed == 0:
+            if last_pivot_direction == 1:
+                for i in range(last_pivot + 1, t + 1):
+                    deviation_rate = (last_pivot_price - cLOW[i]) / last_pivot_price * 100
+                    if deviation_rate >= dev_threshold:
+                        pivots[last_pivot] = PEAK
+                        last_pivot_confirmed = 1
+                        pivot_confirmed_ats[last_pivot] = t
+                        break
+            else:
+                for i in range(last_pivot + 1, t + 1):
+                    deviation_rate = (cHIGH[i] - last_pivot_price) / last_pivot_price * 100
+                    if deviation_rate >= dev_threshold:
+                        pivots[last_pivot] = VALLEY
+                        last_pivot_confirmed = 1
+                        pivot_confirmed_ats[last_pivot] = t
+                        break
 
 
     return pivots, pivot_confirmed_ats
